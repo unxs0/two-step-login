5ef8ef24da64ba432f18222ec44503a640460d5c
Gary Wallis <github@unxs.io>
2017-07-07 19:57:44 -0400

    Converted to Tomcat war tested ok.

--- src/main/java/com/backbase/progfun/Application.java
+++ src/main/java/com/backbase/progfun/Application.java
@@ -2,13 +2,20 @@ package com.backbase.progfun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
